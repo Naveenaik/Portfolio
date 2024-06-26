3367b8d5fc67b66ff77c0c77a89c1631835b1937
Naveena <109600956+Naveenaik@users.noreply.github.com>
2024-06-26 22:37:51 +0530

    image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,7 @@ import p2 from "../assets/p2.png"
 import p3 from "../assets/p3.png"
 import p4 from "../assets/TC.png"
 import p5 from "../assets/todo.png"
+import p6 from "../assets/ecommerce.png"
 const Projects = () => {
 
   useEffect(()=>{
@@ -28,7 +29,7 @@ const Projects = () => {
         <ProjectCard image={p1} btnText={"View"} title={"Library System Management"} description={"A responsive library system app with React, Express/Node.js, and MySQL, featuring book cataloging, user management, and inventory control."} />
         <ProjectCard image={p4} btnText={"Demo"} title={"Temperature Converter using Flutter"} description={"Using flutter widgets I have created temperature converter app"} />
         <ProjectCard image={p5} btnText={"View"} link={"https://mytodofrontend.vercel.app/"} title={"Task Manager"} description={"This app using the MERN stack with JWT-based authentication, featuring user registration, login, task management in MongoDB, and a responsive React interface."} />
-        <ProjectCard image={p1} btnText={"View"} title={"Ecommerce Website"} description={"A website is created to do the software testing using selenium automation tool"} />
+        <ProjectCard image={p6} btnText={"View"} link={"https://ecommerce-software-testing.vercel.app/"} title={"Ecommerce Website"} description={"A website is created to do the software testing using selenium automation tool"} />
       </div>
       
     </div>
