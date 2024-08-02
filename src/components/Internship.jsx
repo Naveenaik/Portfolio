bd23f0825e8f9c2e8f7a6e1bbbc8ff5a84575ce1
Naveenaik <naveenaik5092003@gmail.com>
2024-08-02 06:50:20 +0530

    Changes

--- src/components/Internship.jsx
+++ src/components/Internship.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import InternCard from "../Card/InternCard";
 import BaratIntern from "../assets/0.jpg";
 import TT from "../assets/Certificate.jpg";
+import Ten from "../assets/10seconds.jpg"
 
 const Internship = () => {
   return (
@@ -11,9 +12,9 @@ const Internship = () => {
       </h2>
       <br />
       <div className="grid grid-cols-2 md:grid-cols-5 gap-5">
-      <InternCard image={BaratIntern} title={"App Development "} />
-      <InternCard image={TT} title={"Fullstack  Development"} />
-      <InternCard image={TT} title={"C Programming"} />
+      <InternCard image={BaratIntern} title={"App Development "} link={"https://drive.google.com/file/d/11iPjtKdH8o_o9ius15Gpow7tKwabsNEP/view?usp=sharing"} />
+      <InternCard image={TT} title={"Fullstack  Development"} link={"https://drive.google.com/file/d/1U39d0Qj1Aq-SSHKKxVOrRxqAbnr0anjG/view?usp=sharing"} />
+      <InternCard image={Ten} title={"C Programming"} link={"https://drive.google.com/file/d/1uVZX2_EOeqxOeakCUBn3AfTuu32mnsWt/view?usp=sharing"}/>
       </div>
     </div>
   );
